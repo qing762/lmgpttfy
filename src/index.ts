a2e8f4b00af38c583a8e8ecd6ed1c2a3eec9758f
Gurkan <hello@gurkz.me>
2023-08-05 19:06:20 +0200

    done

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
-import { Injector, Logger } from "replugged";
+import { Injector, Logger, common } from "replugged";
 
-const inject = new Injector();
+const injector = new Injector();
 const logger = Logger.plugin("LMGTFY");
 
 export function start(): void {
-  logger.log("hewwo");
+  logger.log("hewwo from lmgtfy - developed with ♡ in sweden");
+  injectSendMessage();
+}
+
+function injectSendMessage(): void {
+  injector.instead(common.messages, "sendMessage", async (args, fn) => {
+    if (!args[1].content.startsWith("!lmgtfy ")) return fn(...args);
+    const searchString = args[1].content.replace("!lmgtfy ", "");
+    const link = `https://letmegooglethat.com/?q=${searchString.replace(/ /g, "+")}`;
+    args[1].content = link;
+    return fn(...args);
+  });
 }
 
 export function stop(): void {
-  inject.uninjectAll();
+  injector.uninjectAll();
 }
